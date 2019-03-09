20ee1476b758a234ea6d55c5931078955adb5cfb
zc3hd <34337119@qq.com>
2019-03-09 14:39:26 +0800

    xx

--- cmd.js
+++ cmd.js
@@ -6,6 +6,8 @@ var tool = new Tool();
 // console.log
 // console.log
 // console.logD
+// console.logD
+// console.logD
 
 var _path = "./";
 // =================================一键上传本地数据库--->online
